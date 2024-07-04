a0ced9fcdf6a940619ca5e125730b1c36c10aa0d
254binaryninja <arnoldmusandu@gmail.com>
2024-07-04 11:01:48 +0300

    implement stream part 1

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,11 @@
 import React, { ReactNode } from 'react'
+import StreamVideoProvider from '../../Providers/StreamClientProvider'
 
  const RootLayout = ({children}:{children : ReactNode}) => {
   return (
     <main>
-        {children}
-   </main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
   )
 }
 export default RootLayout
